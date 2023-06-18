db8286640bba2fca22f25961367ef7395cd7df5e
koderEngine <67615016+koderEngine@users.noreply.github.com>
2023-06-18 23:30:24 +1000

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -16,15 +16,32 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-# Initializing the LLM and the Toolset
-llm = OpenAI(temperature=0)
-tools = load_tools(["wikipedia"], llm=llm)
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+with st.sidebar:
+  openai_api_key = st.text_input('OpenAI API Key')
+  st.markdown("""
+  ### TIP:
+  1. Enter in your OPENAI Key
+  2. Enter in your question
+  3. Click on the button
+  
+  """)
+
+def generate_response(input):
+  if input:
+    # Initializing the LLM and the Toolset
+    llm = OpenAI(temperature=0, max_tokens=2048,openai_api_key=openai_api_key)
+    tools = load_tools(["wikipedia"], llm=llm)
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    text = agent.run(input)
+    st.info(text)
+
 
 st.title('Request from Wikipedia')
-# Collect the prompt from the user 
-input = st.text_input('Enter your question here')
 
-if input: 
-    text = agent.run(input)
-    st.write(text)
+# Collect the prompt from the user 
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are 3 key advice for learning how to code?', max_chars=2048)
+  submitted = st.form_submit_button('Submit')
+  if submitted:
+    generate_response(text)
+
